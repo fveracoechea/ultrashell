ebb2d5ab499ed64d9fa375c04db5fee692e6b709
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2026-03-21 20:22:40 -0400

    change bluetooth settings

--- widgets/bluetooth/BluetoothDropdown.tsx
+++ widgets/bluetooth/BluetoothDropdown.tsx
@@ -37,7 +37,7 @@ export function BluetoothDropdown() {
 						valign={Gtk.Align.START}
 						onClicked={() => {
 							popover.popdown();
-							execAsync("bluetui");
+							execAsync("ghostty --class=TUI.float -e bluetui");
 						}}
 					>
 						<label label="󰒓" class="icon" />
